441ac64f369969d4e8b0d2ca1bb69b7cd68fea18
nenslen <nicolas.enslen@gmail.com>
2016-10-27 19:51:58 -0600

    First Commit
    First commit

---
+++ FlashCards/Form1.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace FlashCards {
+    partial class frmFlashCards {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // frmFlashCards
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "frmFlashCards";
+            this.Text = "Flash Cards";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
